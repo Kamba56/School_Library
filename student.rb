78fa8ae8d8ae90f787db57486f3dbcaa57d7e3a9
Toma John Musa <tomajohn316@yahoo.com>
2022-09-08 16:22:02 +0100

    Update student.rb

--- student.rb
+++ student.rb
@@ -3,8 +3,8 @@ require_relative './person'
 class Student < Person
   attr_accessor @classroom
 
-  def initialize(classroom)
-    super
+  def initialize(age, classroom, name = "Unknown", parent_permission = true)
+    super(age, name, parent_permission)
     @classroom = classroom
   end
 
