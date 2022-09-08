2c70a93e51eb1cf4bcfcd35069ea5175331d504f
Toma John Musa <tomajohn316@yahoo.com>
2022-09-08 16:19:58 +0100

    Update person.rb

--- person.rb
+++ person.rb
@@ -2,14 +2,15 @@ class Person
   attr_reader @id
   attr_accessor @name, @age
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, id = Random.rand(1..1000), name = 'Unknown', parent_permission: true)
+    @id = id
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
   def can_use_services?
-    is_of_age? || @parent_permission
+    of_age?() || @parent_permission
   end
 
   private
