c52dd0f6a1c95d2840237ecb0322fece987e49bc
toma <tomajohn316@yahoo.com>
2022-09-09 20:50:27 +0100

    Add many to many rental relationship

--- book.rb
+++ book.rb
@@ -1,8 +1,10 @@
 class Book
+  attr_reader :rental
   attr_accessor :title, :author
 
   def initialize(title, author)
     @title = title
     @author = author
+    @rental = []
   end
 end
--- rent.rb
+++ rent.rb
@@ -1,7 +1,12 @@
 class Rental
+  attr_reader :person, :book
   attr_accessor :date
 
-  def initialize(date)
+  def initialize(person, book, date)
     @date = date
+    @book = book
+    @person = person
+    person.rental << self
+    book.rental << self
   end
 end
